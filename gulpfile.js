1ddd79d89cb0d98db1410f122da267e4002265f4
chykehyman <hyginuschinwoke@gmail.com>
2017-11-21 20:07:27 +0100

    chore(gulp): convert grunt task runner to gulp task runner - Create gulpfile.js file - Implement gulp task runner using initial grunt task runner as transformation base - Install required gulp runner npm packages - Modify script commands (grunt to gulp) in package.json file - Git ignore package-lock.json file and bower_components directory - Modify href and src paths to bootstrap css and js files in head.jade and foot.jade files respectively - Delete gruntfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,10 @@ const gulp = require('gulp'),
     eslint = require('gulp-eslint'),
     mocha = require('gulp-mocha');
 
-gulp.task('watch', () => {
-    gulp.watch('app/views/**', browserSync.reload());
-    gulp.watch('public/js/**', browserSync.reload());
-    gulp.watch('app/**/*.js', browserSync.reload());
-    gulp.watch('public/views/**', browserSync.reload());
-    gulp.watch('public/css/common.scss', ['sass']);
-    gulp.watch('public/css/**', browserSync.reload());
-});
 
-gulp.task('eslint', () => {
-    gulp.src(['gulpfile.js',
-            'public/js/**/*.js',
-            'test/**/*.js',
-            'app/**/*.js'
-        ])
-        .pipe(eslint());
+gulp.task('bower', () => {
+    bower().pipe(gulp.dest('./bower_components'));
 });
-
 
 gulp.task('nodemon', () => {
     nodemon({
@@ -37,14 +23,58 @@ gulp.task('nodemon', () => {
     });
 });
 
+gulp.task('watch', () => {
+    gulp.watch('app/views/**', browserSync.reload());
+    gulp.watch('public/js/**', browserSync.reload());
+    gulp.watch('app/**/*.js', browserSync.reload());
+    gulp.watch('public/views/**', browserSync.reload());
+    gulp.watch('public/css/common.scss', ['sass']);
+    gulp.watch('public/css/**', browserSync.reload());
+});
+
 gulp.task('sass', () => {
     gulp.src('public/css/common/scss')
         .pipe(sass())
         .pipe(gulp.dest('public/css/'));
 });
 
-gulp.task('bower', () => {
-    bower().pipe(gulp.dest('./bower_components'));
+gulp.task('lint', () => {
+    gulp.src(['gulpfile.js',
+            'public/js/**/*.js',
+            'test/**/*.js',
+            'app/**/*.js'
+        ])
+        .pipe(eslint());
+});
+
+gulp.task('angular', () => {
+    gulp.src('app/bower_components/angular/**/*.js')
+        .pipe(gulp.dest('public/lib/angular'));
+});
+
+gulp.task('bootstrap', () => {
+    gulp.src('bower_components/bootstrap/**/*')
+        .pipe(gulp.dest('public/lib/bootstrap/dist'));
+});
+
+gulp.task('jquery', () => {
+    gulp.src('bower_components/jquery/**/*')
+        .pipe(gulp.dest('public/lib/jquery'));
+});
+
+gulp.task('underscore', () => {
+    gulp.src('bower_components/underscore/**/*')
+        .pipe(gulp.dest('public/lib/underscore'));
+});
+
+gulp.task('angularUtils', () => {
+    gulp.src('bower_components/angular-ui-utils/modules/route/route.js')
+        .pipe(gulp.dest('public/lib/angular-ui-utils/modules'));
+});
+
+gulp.task('angular-bootstrap', () => {
+    gulp.src('bower_components/angular-bootstrap/**/*')
+        .pipe(gulp.dest('public/lib/angular-bootstrap'));
 });
 
 gulp.task('mochaTest', () => {
@@ -55,5 +85,9 @@ gulp.task('mochaTest', () => {
 });
 
 gulp.task('install', ['bower']);
+
 gulp.task('test', ['mochaTest']);
-gulp.task('default', ['nodemon', 'watch', 'sass']);
+
+gulp.task('default', ['nodemon', 'watch', 'sass', 'angular', 'bootstrap',
+    'jquery', 'underscore', 'angularUtils', 'angular-bootstrap'
+]);
