04c4e8fbb02988b13c0f97ef6cf1b45573a376bf
Osaze <andeladeveloper@Osaze.local>
2017-11-21 13:08:03 +0100

    refactored code

--- unittests/mocktests.js
+++ unittests/mocktests.js
@@ -1,12 +1,12 @@
 // Setup sample tests to confirm functionality of the test environment setup.
 describe('Users factory', () => {
-  it('has a dummy spec to test 2 + 2', () => {
+  it('has a dummy spec to test 3 + 1', () => {
     // An intentionally passing test.
     expect(3 + 1).toEqual(4);
   });
 
-  it('has another dummy spec to test 2 + 2', () => {
-    // An intentionally failing test.
-    expect(34 + 1).toEqual(35);
+  it('has another dummy spec to test 17 + 8', () => {
+    // An intentionally passing test.
+    expect(17 + 8).toEqual(25);
   });
 });
