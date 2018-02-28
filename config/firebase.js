83c4ce9fafa502df0b2f3a50788ac0950c134189
daddychuks <durugo_chuks@yahoo.com>
2018-02-28 17:23:22 +0100

    update firebase url

--- config/firebase.js
+++ config/firebase.js
@@ -5,7 +5,7 @@ dotenv.config();
 const config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
+  databaseURL: process.env.FIREBASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID
